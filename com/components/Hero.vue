816044df8414bf96175b8d18e12b11b677336c86
Pedro Bernardi Alves <pedrobealves@gmail.com>
2018-05-13 22:09:27 -0300

    Test vue.js

---
+++ com/components/Hero.vue
@@ -0,0 +1,18 @@
+<template>
+    <img
+            class="hero__avatar"
+            src="/img/avatar2.png"
+            :alt="hero.name">
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    .hero {
+        width: 300px;
+        background: green;
+        border-radius: 5px;
+    }
+</style>
